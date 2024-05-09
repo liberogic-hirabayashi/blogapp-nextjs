9687dbbe02f8fd8d1dc140b6278d6882877b7f14
平林真奈 <>
2024-05-09 10:50:50 +0900

    ディレクトリ変更

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { main } from "../route";
+import { main } from "../../route";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 async function fetchAllBlogs(){
-  const res = await fetch('http://localhost:3002/api/blog',{cache:"no-store"})
+  const res = await fetch('http://localhost:3002/api',{cache:"no-store"})
   const data = await res.json();
   return data.posts;
 }
